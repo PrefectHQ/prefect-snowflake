f2dc3526aaf7c7255aaa3114a2e8bbb0a524f2c4
ahuang11 <andrew.h@prefect.io>
2022-08-11 11:15:20 -0700

    Remove database warehouse kwargs

--- prefect_snowflake/database.py
+++ prefect_snowflake/database.py
@@ -18,8 +18,6 @@ async def snowflake_query(
     snowflake_connector: "SnowflakeConnector",
     params: Union[Tuple[Any], Dict[str, Any]] = None,
     cursor_type: Optional[SnowflakeCursor] = SnowflakeCursor,
-    database: Optional[str] = None,
-    warehouse: Optional[str] = None,
 ) -> List[Tuple[Any]]:
     """
     Executes a query against a Snowflake database.
@@ -29,10 +27,6 @@ async def snowflake_query(
         params: The params to replace the placeholders in the query.
         snowflake_connector: The credentials to use to authenticate.
         cursor_type: The type of database cursor to use for the query.
-        database: The name of the database to use; overrides
-            the credentials definition if provided.
-        warehouse: The name of the warehouse to use; overrides
-            the credentials definition if provided.
 
     Returns:
         The output of `response.fetchall()`.
@@ -51,8 +45,6 @@ async def snowflake_query(
                 account="account",
                 user="user",
                 password="password",
-                database="database",
-                warehouse="warehouse",
             )
             result = snowflake_query(
                 "SELECT * FROM table WHERE id=%{id_param}s LIMIT 8;",
@@ -64,9 +56,8 @@ async def snowflake_query(
         snowflake_query_flow()
         ```
     """
-    connect_params = {"database": database, "warehouse": warehouse}
     # context manager automatically rolls back failed transactions and closes
-    with snowflake_connector.get_connection(**connect_params) as connection:
+    with snowflake_connector.get_connection() as connection:
         with connection.cursor(cursor_type) as cursor:
             response = cursor.execute_async(query, params=params)
             query_id = response["queryId"]
@@ -85,8 +76,6 @@ async def snowflake_multiquery(
     snowflake_connector: "SnowflakeConnector",
     params: Union[Tuple[Any], Dict[str, Any]] = None,
     cursor_type: Optional[SnowflakeCursor] = SnowflakeCursor,
-    database: Optional[str] = None,
-    warehouse: Optional[str] = None,
     as_transaction: bool = False,
     return_transaction_control_results: bool = False,
 ) -> List[List[Tuple[Any]]]:
@@ -99,10 +88,6 @@ async def snowflake_multiquery(
         params: The params to replace the placeholders in the query.
         snowflake_connector: The credentials to use to authenticate.
         cursor_type: The type of database cursor to use for the query.
-        database: The name of the database to use; overrides
-            the credentials definition if provided.
-        warehouse: The name of the warehouse to use; overrides
-            the credentials definition if provided.
         as_transaction: If True, queries are executed in a transaction.
         return_transaction_control_results: Determines if the results of queries
             controlling the transaction (BEGIN/COMMIT) should be returned.
@@ -124,8 +109,6 @@ async def snowflake_multiquery(
                 account="account",
                 user="user",
                 password="password",
-                database="database",
-                warehouse="warehouse",
             )
             result = snowflake_multiquery(
                 ["SELECT * FROM table WHERE id=%{id_param}s LIMIT 8;", "SELECT 1,2"],
@@ -138,8 +121,7 @@ async def snowflake_multiquery(
         snowflake_multiquery_flow()
         ```
     """
-    connect_params = {"database": database, "warehouse": warehouse}
-    with snowflake_connector.get_connection(**connect_params) as connection:
+    with snowflake_connector.get_connection() as connection:
         if as_transaction:
             queries.insert(0, BEGIN_TRANSACTION_STATEMENT)
             queries.append(END_TRANSACTION_STATEMENT)
